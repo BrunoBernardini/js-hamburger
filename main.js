4d20d089af9d14ac21e2a260c3033f18405d8dd2
BrunoBernardini <bruno.f.bernardini@gmail.com>
2022-04-07 19:19:47 +0200

    Chiusura menù.

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 const hamburgerBtn = document.querySelector(".header-right > a");
+const closeBtn = document.querySelector(".hamburger-menu > a");
 const hamburgerMenu = document.querySelector(".hamburger-menu");
-hamburgerBtn.addEventListener('click',function(){
-  hamburgerMenu.classList.add('active');
+hamburgerBtn.addEventListener("click", function(){
+  hamburgerMenu.classList.add("active");
+});
+closeBtn.addEventListener("click", function(){
+  hamburgerMenu.classList.remove("active");
 });
